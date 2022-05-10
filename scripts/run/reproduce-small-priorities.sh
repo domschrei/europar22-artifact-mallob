f6b176e9e620a9d2a7d9e90277c00f6a42cbb875
Dominik Schreiber <mail@dominikschreiber.de>
2022-05-10 09:56:42 +0200

    Change handling of job templates for uniform experiments

--- scripts/run/reproduce-small-priorities.sh
+++ scripts/run/reproduce-small-priorities.sh
@@ -5,7 +5,7 @@ set -e
 echo "******************************************************"
 echo "Running experiment \"Impact of Priorities\""
 
-PATH=build:$PATH RDMAV_FORK_SAFE=1 mpirun -np 32 -map-by numa:PE=1 -bind-to core build/mallob -t=1 -q -c=4 -ajpc=1 -jwl=60 -T=3600 -log=logs/priorities -v=4 -warmup -pls=0 -shuffle-job-descriptions -job-template=templates/job-template-priorities.json -job-desc-template=templates/selection_isc2020.txt 2>&1 > OUT
+PATH=build:$PATH RDMAV_FORK_SAFE=1 mpirun -np 32 -map-by numa:PE=1 -bind-to core build/mallob -t=1 -q -c=4 -ajpc=1 -jwl=60 -T=3600 -log=logs/priorities -v=4 -warmup -pls=0 -shuffle-job-descriptions -job-template=templates/job-template-priorities.json -job-desc-template=templates/selection_isc2020.txt 2>&1 > OUT < /dev/null
 
 echo "Experiment done"
 echo "******************************************************"
--- scripts/run/reproduce-small-uniform.sh
+++ scripts/run/reproduce-small-uniform.sh
@@ -11,10 +11,14 @@ cat scripts/run/configs-small-uniform.csv|while read -r line; do
     loadedjobsperclient=$(echo $line|awk '{print $5}')
     jobtemplate=templates/job-template-sat-r3unknown_100k-${coreminperjob}coremin.json
     
+    if [ ! -f $jobtemplate ]; then
+        cat templates/job-template-sat-r3unknown_100k.json|sed 's/CPUMINUTES/'$coreminperjob'/g' > $jobtemplate
+    fi
+    
     echo "******************************************************"
     echo "Running experiment: npar=$npar coreminperjob=$coreminperjob numclients=$numclients activejobsperclient=$activejobsperclient loadedjobsperclient=$loadedjobsperclient"
     
-    PATH=build:$PATH RDMAV_FORK_SAFE=1 mpirun -np 32 -map-by numa:PE=1 -bind-to core build/mallob -t=1 -q -c=$numclients -ajpc=$activejobsperclient -ljpc=$loadedjobsperclient -T=320 -log=logs/uniform-$npar -v=4 -warmup -job-template=$jobtemplate 2>&1 > OUT
+    PATH=build:$PATH RDMAV_FORK_SAFE=1 mpirun -np 32 -map-by numa:PE=1 -bind-to core build/mallob -t=1 -q -c=$numclients -ajpc=$activejobsperclient -ljpc=$loadedjobsperclient -T=320 -log=logs/uniform-$npar -v=4 -warmup -job-template=$jobtemplate 2>&1 > OUT < /dev/null
     
     echo "Experiment done"
     echo "******************************************************"
