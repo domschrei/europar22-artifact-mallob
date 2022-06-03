de10ca3ff86c0c30caf55fe79149757f8cf4a06e
Dominik Schreiber <mail@dominikschreiber.de>
2022-06-03 11:40:54 +0200

    Improve process of fetching benchmarks

--- instances/fetch_sat2020_instances.sh
+++ instances/fetch_sat2020_instances.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-wget https://satcompetition.github.io/2020/downloads/sc2020-main.uri
+# Download formulae one by one
 wget --content-disposition -i sc2020-main.uri
 
+# For each downloaded compressed file:
 for f in *.cnf.xz; do 
     echo "$f"
+    
+    # Fix for older versions of wget: remove prefix of
+    # the original filename (i.e., 32-digit hex number)
+    if echo $f|grep -qE "^[0-9a-f]{32}-.*"; then
+        destf=$(echo $f|cut -c 34-)
+        mv $f $destf
+        f=$destf
+    fi
+    
+    # Decompress file
     unxz "$f"
 done
